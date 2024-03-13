39b8f971404d4fa77b449f281755ca42c9d92927
Adrian L Thomas <AdrianLThomas@users.noreply.github.com>
2024-03-13 13:11:31 +0000

    fix: lint

--- test/integration/create-url.test.ts
+++ test/integration/create-url.test.ts
@@ -1,22 +1,3 @@
-// import { unstable_dev } from "wrangler";
-// import { UnstableDevWorker } from "wrangler";
-
-// describe("Worker", () => {
-//   let worker: UnstableDevWorker;
-
-//   beforeAll(async () => {
-//     worker = await unstable_dev("src/index.ts", {
-//       experimental: { disableExperimentalWarning: true },
-//     });
-//   });
-
-//   afterAll(async () => {
-//     await worker.stop();
-//   });
-
-//   it("should return a 404 for an invalid route", async () => {
-//     const resp = await worker.fetch()
-//     const text = await resp.text();
-//     expect(text).toMatchInlineSnapshot(`"Not found"`);
-//   });
-// });
+it('should be true', () => {
+	expect(true).toBe(true);
+});
