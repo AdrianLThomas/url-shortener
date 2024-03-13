3b66e06f0de84f690539320ef3536f94665cdbf9
Adrian L Thomas <AdrianLThomas@users.noreply.github.com>
2024-03-13 15:37:54 +0000

    test: outlined test scenarios for url shortening

--- test/integration/create-url.test.ts
+++ test/integration/create-url.test.ts
@@ -23,4 +23,11 @@ describe('Worker', () => {
 			shortUrl: 'Yzk4NG',
 		});
 	});
+
+	// should return 400 for missing url
+	// should return 400 for invalid url
+	// should return the same url if it is already shortened (x2 times)
+	// should return a shortened url for a very long url
+	// should return a shortened url for a url with special characters
+	// should return 400 if attempting to shorten an already shortened url
 });
